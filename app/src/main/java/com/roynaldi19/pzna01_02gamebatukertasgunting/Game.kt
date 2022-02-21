03bc0ae0998abb65249e5ad354541103e52078f3
roynaldi19 <roynaldi19@gmail.com>
2022-02-21 15:18:16 +0700

    selesai

---
+++ app/src/main/java/com/roynaldi19/pzna01_02gamebatukertasgunting/Game.kt
@@ -0,0 +1,32 @@
+package com.roynaldi19.pzna01_02gamebatukertasgunting
+
+import kotlin.random.Random
+
+
+object Game {
+
+    private val rules = mapOf(
+        "ROCK-SCISSORS" to true,
+        "ROCK-PAPER" to false,
+        "SCISSORS-PAPER" to true,
+        "SCISSORS-ROCK" to false,
+        "PAPER-ROCK" to true,
+        "PAPER-SCISSORS" to false,
+    )
+
+    private val options = listOf("ROCK", "SCISSORS", "PAPER")
+
+    private val optionsDrawable = mapOf(
+        "ROCK" to R.drawable.batu,
+        "PAPER" to R.drawable.kertas,
+        "SCISSORS" to R.drawable.gunting
+    )
+
+    fun pickRandomOption(): String = options[Random.nextInt(0, 3)]
+
+    fun pickDrawable(option: String): Int = optionsDrawable[option]!!
+
+    fun isDraw(from: String, to: String): Boolean = from == to
+
+    fun isWin(from: String, to: String): Boolean = rules["$from-$to"]!!
+}
--- app/src/main/java/com/roynaldi19/pzna01_02gamebatukertasgunting/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/pzna01_02gamebatukertasgunting/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.roynaldi19.pzna01_02gamebatukertasgunting
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cpuImgV: ImageView
+    lateinit var batuImgB: ImageButton
+    lateinit var guntingImgB: ImageButton
+    lateinit var kertasImgB: ImageButton
+    lateinit var resultImgV: ImageView
+
+    private fun initComponent() {
+        cpuImgV = findViewById(R.id.cpu_img)
+        batuImgB = findViewById(R.id.batu_ib)
+        guntingImgB = findViewById(R.id.gunting_ib)
+        kertasImgB = findViewById(R.id.kertas_ib)
+        resultImgV = findViewById(R.id.result_img)
+    }
+
+    private fun initListener() {
+        batuImgB.setOnClickListener { startGame("ROCK") }
+        guntingImgB.setOnClickListener { startGame("SCISSORS") }
+        kertasImgB.setOnClickListener { startGame("PAPER") }
+    }
+
+    private fun startGame(option: String) {
+
+        val cpuOption = Game.pickRandomOption()
+        cpuImgV.setImageResource(Game.pickDrawable(cpuOption))
+
+        when{
+            Game.isDraw(option, cpuOption) -> resultImgV.setImageResource(R.drawable.coba)
+            Game.isWin(option, cpuOption) -> resultImgV.setImageResource(R.drawable.win)
+            else -> resultImgV.setImageResource(R.drawable.lose)
+        }
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponent()
+        initListener()
     }
 }
